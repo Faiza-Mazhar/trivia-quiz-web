5a2ed1a805b49dcf19aeb97991d344c68b59fd77
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2021-01-05 16:06:24 +0000

    Added axios and made network call to api to fetch data for Categories

---
+++ src/Containers/sidebar/helper.js
@@ -0,0 +1,5 @@
+const numQuestion = ["Any", "5", "10", "15"];
+const difficultyLevel = ["Any", "Easy", "Medium", "Hard"];
+const questionType = ["Any", "Multiple Choice", "True/False"];
+
+export { numQuestion, difficultyLevel, questionType };
