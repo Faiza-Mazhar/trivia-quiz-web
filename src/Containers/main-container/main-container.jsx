948369710b9fb00785973b323809e1019da97ca3
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2021-01-07 14:31:12 +0000

    Changed styles, added the quiz container

--- src/Containers/main-container/main-container.jsx
+++ src/Containers/main-container/main-container.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import Sidebar from "../sidebar/sidebar";
+import QuizDisplay from "../QuizDisplay/quiz-display";
+
+import "./main-container.style.scss";
 
 const MainContainer = () => {
   const [quizQueryParams, setQuizQueryParams] = useState();
+
+  let initialString = () =>
+    quizQueryParams ? "Play Quiz" : "Please select Quiz options and hit play";
+
   return (
     <div className="main-container">
-      <Sidebar setQuizQueryParams={setQuizQueryParams} />
+      <Sidebar setQuizQueryParams={setQuizQueryParams} className="sidebar" />
+      <QuizDisplay initialString={initialString()} className="quiz-display" />
     </div>
   );
 };
