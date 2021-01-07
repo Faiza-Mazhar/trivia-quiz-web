0e5c9f7ff40d9b1aabf0337ce13c989a923898d9
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2021-01-07 11:40:22 +0000

    Added test that UI is updated with the api fetched categories

---
+++ src/tests/Containers/sidebar/sidebar.test.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import Sidebar from "../../../Containers/sidebar/sidebar";
+import {
+  fetchCategoryData,
+  getCategoryInfo,
+} from "../../../Containers/sidebar/helper";
+
+import { act, render, cleanup, waitFor } from "@testing-library/react";
+
+afterEach(cleanup);
+jest.mock("../../../Containers/sidebar/helper");
+
+const categories = {
+  trivia_categories: [
+    {
+      id: 9,
+      name: "General Knowledge",
+    },
+    {
+      id: 10,
+      name: "Entertainment: Books",
+    },
+  ],
+};
+test("should have fetched categories information and populate the category dropdown on loading the page", async () => {
+  await fetchCategoryData.mockResolvedValue(categories.trivia_categories);
+
+  getCategoryInfo.mockImplementationOnce(() => ({
+    categoriesInfo: ["General Knowledge", "Entertainment: Books"],
+    categoriesIdsInfo: [9, 10],
+  }));
+
+  await act(async () => {
+    const { getByText } = render(<Sidebar />);
+
+    await waitFor(() => getByText("General Knowledge"));
+
+    expect(await fetchCategoryData).toHaveBeenCalled();
+    expect(await fetchCategoryData).toHaveBeenCalledTimes(1);
+
+    expect(getByText(/Select Category/i).textContent).toBe(
+      "Select CategoryAnyGeneral KnowledgeEntertainment: Books"
+    );
+  });
+});
