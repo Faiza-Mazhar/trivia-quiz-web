6afaa8ccb7fa88bb794b66fd1309fae72984d9de
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2021-01-11 21:44:25 +0000

    rafactor

--- src/Components/RadioButton/radio-button.jsx
+++ src/Components/RadioButton/radio-button.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./radio-button.style.scss";
 const RadioButton = ({ name, value, onClick }) => {
-  console.log({});
   return (
     <label className="card">
       {value}
