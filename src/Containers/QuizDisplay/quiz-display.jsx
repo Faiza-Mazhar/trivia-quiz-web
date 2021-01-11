6cc942cd719d5758802d46355dc466cfda635ac9
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2021-01-11 13:20:37 +0000

    created a form with button to submit right answer - wip

--- src/Containers/QuizDisplay/quiz-display.jsx
+++ src/Containers/QuizDisplay/quiz-display.jsx
@@ -1,13 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./quiz-display.style.scss";
 
 import QuestionHeader from "../../Components/QuestionHeader/question-header";
-import { decodeHtmlEntities } from "../../helpers/fetcher";
+import CustomButton from "../../Components/CustomButton/custom-button";
+import { shuffleAnswers, decodeHtmlEntities, getReply } from "./helper";
 
 const QuizDisplay = ({ quizQuestions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [answers, setAnswers] = useState();
 
-  const { category, difficulty, question } = quizQuestions[currentQuestion];
+  const [replyString, setReplyString] = useState(undefined);
+
+  const {
+    category,
+    difficulty,
+    question,
+    correct_answer,
+    incorrect_answers,
+  } = quizQuestions[currentQuestion];
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (selectedAnswer === "") {
+      alert("Please select an answer");
+      return;
+    }
+
+    if (selectedAnswer !== correct_answer) {
+      setReplyString(getReply(correct_answer));
+    } else {
+      setReplyString(getReply());
+    }
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setSelectedAnswer(event.target.value);
+  };
+
+  const nextQuestion = () => {
+    if (currentQuestion < quizQuestions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+      setReplyString("");
+    }
+  };
+  useEffect(() => {
+    !answers && setAnswers(shuffleAnswers(correct_answer, incorrect_answers));
+  }, [answers, correct_answer, incorrect_answers]);
+
   return (
     <div className="quiz-container">
       <QuestionHeader
@@ -19,6 +61,36 @@ const QuizDisplay = ({ quizQuestions }) => {
       />
 
       <div className="question">{decodeHtmlEntities(question)}</div>
+
+      <form onSubmit={handleSubmit}>
+        <div className="answers-container">
+          {answers &&
+            answers.map((answer) => {
+              const decodedAnswer = decodeHtmlEntities(answer);
+              return (
+                <div key={decodedAnswer} className="answer-container">
+                  <CustomButton
+                    isAnswerButton={true}
+                    value={decodedAnswer}
+                    id={decodedAnswer}
+                    onClick={handleChange}
+                  >
+                    {decodedAnswer}
+                  </CustomButton>
+                </div>
+              );
+            })}
+        </div>
+
+        <div className="submit-button">
+          {!replyString && <CustomButton type="submit">SUBMIT</CustomButton>}
+          {replyString && (
+            <CustomButton onClick={nextQuestion}>Next</CustomButton>
+          )}
+        </div>
+      </form>
+
+      {replyString && <label>{replyString}</label>}
     </div>
   );
 };
--- src/helpers/fetcher.js
+++ src/helpers/fetcher.js
@@ -9,13 +9,4 @@ const fetchData = (url) => {
     });
 };
 
-const decodeHtmlEntities = (str) => {
-  return String(str)
-    .replace(/&amp;/g, "&")
-    .replace(/&lt;/g, "<")
-    .replace(/&gt;/g, ">")
-    .replace(/&quot;/g, '"')
-    .replace(/&#039;/g, "'");
-};
-
-export { fetchData, decodeHtmlEntities };
+export { fetchData };
