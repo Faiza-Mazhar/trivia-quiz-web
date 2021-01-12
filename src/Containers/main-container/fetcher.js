7ef4d317f87856a04fef6c9ea53860ec23bf145f
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2021-01-12 14:10:41 +0000

    Moved the logic to decode enities in the fetcher

--- src/Containers/main-container/fetcher.js
+++ src/Containers/main-container/fetcher.js
@@ -3,8 +3,54 @@ import { fetchData } from "../../helpers/fetcher";
 const fetchQuestions = async (quizQueryParams) => {
   const url = generateQuizUrl(quizQueryParams);
   const response = await fetchData(url);
+  return decodeData(response.results);
+};
 
-  return response.results;
+const decodeData = (data) => {
+  return data.map((questionItem) => {
+    const correctAnswer = decodeHtmlEntities(questionItem.correct_answer);
+    const answers = shuffleAnswers(
+      correctAnswer,
+      questionItem.incorrect_answers
+    );
+    const { category, type, difficulty, question } = questionItem;
+    return {
+      category: category,
+      type: type,
+      difficulty: difficulty,
+      question: decodeHtmlEntities(question),
+      correctAnswer: correctAnswer,
+      answers: answers,
+    };
+  });
+};
+
+const decodeHtmlEntities = (str) => {
+  return String(str)
+    .replace(/&amp;/g, "&")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'")
+    .replace(/&uuml;/g, "ü")
+    .replace(/&ntilde;/g, "ñ")
+    .replace(/&rsquo;/g, "'");
+};
+
+const shuffleAnswers = (correctAnswer, wrongAnswers) => {
+  if (wrongAnswers.length < 2) {
+    return ["True", "False"];
+  }
+  const answers = [
+    correctAnswer,
+    ...wrongAnswers.map((answer) => decodeHtmlEntities(answer)),
+  ];
+  for (let i = answers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [answers[i], answers[j]] = [answers[j], answers[i]];
+  }
+
+  return answers;
 };
 
 const generateQuizUrl = ({
