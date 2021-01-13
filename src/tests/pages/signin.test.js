4b38ad2780ca811102d2a49038bbdc00ee6c516f
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2021-01-13 19:53:46 +0000

    added signina and sign up page testing

---
+++ src/tests/pages/signin.test.js
@@ -0,0 +1,41 @@
+import SignInAndSignUpPage from "../../pages/signin-signup/signin-signup";
+
+import {
+  act,
+  render,
+  waitFor,
+  screen,
+  fireEvent,
+  cleanup,
+} from "@testing-library/react";
+
+test("On loading sign in page, SignIn component is rendered", () => {
+  render(<SignInAndSignUpPage />);
+
+  expect(
+    screen.getByText("Sign in with your email and password.")
+  ).toBeInTheDocument();
+});
+
+test("If user press New User label, the SignOut component is rendered", async () => {
+  const { getByText } = render(<SignInAndSignUpPage />);
+  await waitFor(() => getByText("New User?"));
+  fireEvent.click(getByText("New User?"));
+  expect(
+    screen.getByText("Sign up with your email and password.")
+  ).toBeInTheDocument();
+});
+
+test("If user press - Already have an account- label, the SignOut component is rendered", async () => {
+  const { getByText } = render(<SignInAndSignUpPage />);
+
+  await waitFor(() => getByText("New User?"));
+  fireEvent.click(getByText("New User?"));
+
+  await waitFor(() => getByText("Already have an account?"));
+  fireEvent.click(getByText("Already have an account?"));
+
+  expect(
+    screen.getByText("Sign in with your email and password.")
+  ).toBeInTheDocument();
+});
