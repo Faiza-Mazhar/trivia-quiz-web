2f0461e6ed49ddcea561d43ef64d79734e0695fc
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2021-01-26 09:37:32 +0000

    Updated the heading of props

--- src/Components/Dropdown/use-dropdown.jsx
+++ src/Components/Dropdown/use-dropdown.jsx
@@ -3,7 +3,7 @@ import "./use-dropdown.style.scss";
 
 const useDropdown = (label, defaultState, options) => {
   const [state, updateState] = useState(defaultState);
-  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
+  const id = `use-dropdown-${label.replace(" ", "-").toLowerCase()}`;
   const Dropdown = () => (
     <div className="dropdown-container">
       <label htmlFor={id} className="dropdown-label">
